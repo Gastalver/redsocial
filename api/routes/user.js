fb7a6db5e2e03cc4ad198ae44d6f4177f792ffb0
Gastalver <mgastalver@gmail.com>
2018-05-04 20:43:58 +0200

    Implementado upload-image-user

--- api/routes/user.js
+++ api/routes/user.js
@@ -16,8 +16,9 @@ api.get('/home',UserController.home);
 api.get('/pruebas', md_auth.ensureAuth,UserController.pruebas);
 api.get('/user/:id',md_auth.ensureAuth,UserController.getUser);
 api.get('/users/:page?',md_auth.ensureAuth,UserController.getUsers);
+api.get('/get-image-user/:imageFile',md_auth.ensureAuth, UserController.getImageFile); // Más rápido sin autenticación
 api.post('/register',UserController.saveUser);
 api.post('/login',UserController.loginUser);
 api.put('/update-user/:id',md_auth.ensureAuth,UserController.updateUsers);
-api.post('upload-image-user/:id',[md_auth.ensureAuth,md_upload],UserController.uploadImage);
+api.post('/upload-image-user/:id',[md_auth.ensureAuth,md_upload],UserController.uploadImage);
 module.exports = api;
