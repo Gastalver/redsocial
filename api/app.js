9f3750a0ab54002892fe0c37e64fc2244a3c8e23
Gastalver <mgastalver@gmail.com>
2018-05-05 23:48:22 +0200

    Añadidos archivos controlador y rutas de publicaciones.

--- api/app.js
+++ api/app.js
@@ -7,6 +7,8 @@ var app = express();
 // cargar rutas
 var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
+var publication_routes = require('./routes/publication');
+
 
 // middleware
     // exigido por bodyparser
@@ -19,6 +21,7 @@ var follow_routes = require('./routes/follow');
 // rutas
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
+app.use('/api', publication_routes);
 
 
 //exportar
